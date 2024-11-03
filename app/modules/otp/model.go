55823f2242397d6b2dacb86a590dc1297535ff78
Tommy <tommyubongabasijohn@gmail.com>
2024-11-03 17:46:40 +0100

    add composite index

--- app/modules/otp/model.go
+++ app/modules/otp/model.go
@@ -10,9 +10,9 @@ import (
 type Otp struct {
 	gorm.Model `json:"-"`
 	ID         uuid.UUID      `gorm:"type:uuid;primary_key;default:uuid_generate_v4()" json:"id"`
-	Value      string         `gorm:"not null;index:idx_otp_value_user_case" json:"value"`
-	UserID     uuid.UUID      `gorm:"type:uuid;not null;index:idx_otp_value_user_case" json:"user_id"`
-	UseCase    string         `gorm:"not null;index:idx_otp_value_user_case" json:"use_case"`
+	Value      string         `gorm:"not null;uniqueIndex:,composite:value_user_id_use_case" json:"value"`
+	UserID     uuid.UUID      `gorm:"not null;uniqueIndex:,composite:value_user_id_use_case" json:"user_id"`
+	UseCase    string         `gorm:"not null;uniqueIndex:,composite:value_user_id_use_case" json:"use_case"`
 	ExpiresAt  time.Time      `gorm:"not null;type:TIMESTAMP;" json:"expires_at"`
 	CreatedAt  time.Time      `gorm:"not null;type:TIMESTAMP;" json:"created_at"`
 	UpdatedAt  time.Time      `gorm:"not null;type:TIMESTAMP;" json:"updated_at"`
