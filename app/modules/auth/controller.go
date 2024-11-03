ced81f4b90849344aed5a55e535cf98e95cf3b2a
Tommy <tommyubongabasijohn@gmail.com>
2024-11-03 02:00:35 +0100

    login controller

--- app/modules/auth/controller.go
+++ app/modules/auth/controller.go
@@ -35,3 +35,24 @@ func (a *authController) SignUp(ctx *gin.Context) {
 
 	utils.JsonSuccessResponse(ctx, http.StatusCreated, "signup successful", res)
 }
+
+func (a *authController) Login(ctx *gin.Context) {
+	var params LoginRequest
+
+	if !utils.ValidParams(ctx, &params) {
+		return
+	}
+
+	if !utils.IsValidEmail(params.Email) {
+		utils.JsonErrorResponse(ctx, http.StatusBadRequest, "login unsuccessful", ErrInvalidEmail.Error())
+		return
+	}
+
+	res, err := a.authService.Login(params)
+	if err != nil {
+		utils.JsonErrorResponse(ctx, http.StatusBadRequest, "login unsuccessful", err.Error())
+		return
+	}
+
+	utils.JsonSuccessResponse(ctx, http.StatusOK, "login successful", res)
+}
