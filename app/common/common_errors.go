7a61dbedc4614f407f6505a0953666b08aafbbdd
Tommy <tommyubongabasijohn@gmail.com>
2024-11-17 19:01:21 +0100

    test add, get and delete bookmarks

--- app/common/common_errors.go
+++ app/common/common_errors.go
@@ -3,5 +3,6 @@ package commons
 import "errors"
 
 var (
-	ErrResourceNotFound = errors.New("resource not found")
+	ErrResourceNotFound      = errors.New("resource not found")
+	ErrBookAlreadyBookmarked = errors.New("book already bookmarked")
 )
